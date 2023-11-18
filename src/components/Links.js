f5ff90a2691db91cbc0422d87073c8d8895844cf
Isaac <govegeku00@gmail.com>
2023-11-18 17:13:40 +0300

    Completed Lab

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,6 +5,7 @@ class Links extends React.Component {
   return (
     <div>
       <h3>Links</h3>
+      
       <a href={this.props.github}>{this.props.github}</a>
       <a href={this.props.linkedin}>{this.props.linkedin}</a>
     </div>
